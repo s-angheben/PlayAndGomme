038e5afb5fa01afeafbba0035fb4f2d4b7eff69d
s-angheben <samuele.angheben@gmail.com>
2022-05-13 10:33:29 +0200

    add base connection to the database

---
+++ index.js
@@ -0,0 +1,20 @@
+const app = require('./app/app.js');
+const mongoose = require('mongoose');
+
+const port = 8080;
+const db_url = 'db_url';
+
+
+app.locals.db = mongoose.connect(db_url)
+.then ( () => {
+        
+    app.listen(port, () => {
+        console.log(`Server listening on port: ${port}`);
+    });
+    
+}).catch ( () => {
+
+    console.log("Error connnecting to the database");
+    throw new Error('db connection error');
+
+});
