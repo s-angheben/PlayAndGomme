06ed2c6df07144d79515f5c92d9bec0456b8966b
s-angheben <samuele.angheben@gmail.com>
2022-05-20 18:39:25 +0200

    add dotenv and the base for writing API documentation
    dotenv to load information
    basic swagger setup so the team can write the API

--- app/app.js
+++ app/app.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+    definition: {
+        info: {
+        title: 'PlayAndGomme API Information',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./app/app.js'], // files containing annotations as above
+};
+
+const swaggerDocument = swaggerJsDoc(swaggerOptions);
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
