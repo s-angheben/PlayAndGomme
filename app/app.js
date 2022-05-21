3558ef1da7fc048e9bf6a0074d3ade051ca93c4d
s-angheben <samuele.angheben@gmail.com>
2022-05-21 11:30:57 +0200

    A pre-main branch to merge and test
    A pre-main branch to merge and test the code before push to main

--- app/app.js
+++ app/app.js
@@ -10,7 +10,7 @@ const swaggerOptions = {
             version: '1.0.0',
         },
     },
-    apis: ['./app/app.js'], // files containing annotations as above
+    apis: ['./app/app.js'],
 };
 
 const swaggerDocument = swaggerJsDoc(swaggerOptions);
