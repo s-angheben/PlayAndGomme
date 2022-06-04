2579c8e47f7eb116c647998b103ce5c9ef59342b
s-angheben <samuele.angheben@gmail.com>
2022-06-04 11:13:49 +0200

    update responses

--- app/tokenHandler.js
+++ app/tokenHandler.js
@@ -6,18 +6,18 @@ const jwt = require('jsonwebtoken');
 
 router.post('',async (req,res) =>{
     if (req.body.username == '') {
-        return res.status(400).json({ success: false, message: 'Authentication failed. Missing username field' });
+        return res.status(400).json({ error: 'Authentication failed. Missing username field' });
     }
     if (req.body.password == '') {
-        return res.status(400).json({ success: false, message: 'Authentication failed. Missing password field' });
+        return res.status(400).json({ error: 'Authentication failed. Missing password field' });
     }
     let userExist = await User.findOne({username : req.body.username});
 
     if (userExist == null) {
-        return res.status(404).json({ success: false, message: 'Authentication failed. Incorrect username.' });
+        return res.status(404).json({ error: 'Authentication failed. Incorrect username.' });
     }
     if(userExist.password != req.body.password){
-        return res.status(400).json({ success: false, message: 'Authentication failed. Incorrect password.' });
+        return res.status(400).json({ error: 'Authentication failed. Incorrect password.' });
     }
 
     var payload = {
