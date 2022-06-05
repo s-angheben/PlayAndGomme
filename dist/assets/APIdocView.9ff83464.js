2ae04c3360a1c26915a517495a4c54b27968e7d8
s-angheben <samuele.angheben@gmail.com>
2022-06-05 20:41:18 +0200

    fix vue

--- dist/assets/APIdocView.9ff83464.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,o as c,c as o,a as e}from"./index.8c080956.js";const t={},r={class:"about"},s=e("h1",null,[e("a",{href:"/api-doc",target:"_blank"},[e("u",null,"Clicca qui per aprire la documentazione delle API")])],-1),n=[s];function _(l,i){return c(),o("div",r,n)}var u=a(t,[["render",_]]);export{u as default};
--- dist/assets/BasicUIView.7347952b.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,o as t,c,a as e}from"./index.8c080956.js";const o={},s={class:"about"},n=e("h1",null,[e("a",{href:"/home.html",target:"_blank"},[e("u",null,"Clicca qui per aprire l'interfaccia base del sito (non in VUE)")])],-1),r=[n];function i(_,l){return t(),c("div",s,r)}var f=a(o,[["render",i]]);export{f as default};
