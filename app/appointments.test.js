9b1c5ab6718d9b64ed127937f53e52942b9fe9a6
Christian <christian.moiola@studenti.unitn.it>
2022-06-05 12:03:43 +0200

    Fix appointments.test.js

--- app/appointments.test.js
+++ app/appointments.test.js
@@ -1,11 +1,12 @@
 const request = require('supertest');
 const app = require('./app');
+const jwt = require('jsonwebtoken');
 
 describe('GET /api/v2/appointments', () => {
 
     let appointmentSpy;
     let appointmentSpyFindById;
-  
+
     beforeAll( () => {
       const Appointment = require('./models/appointment');
 
@@ -41,10 +42,23 @@ describe('GET /api/v2/appointments', () => {
       appointmentSpy.mockRestore();
       appointmentSpyFindById.mockRestore();
     });
-    
+
+    var payload = {
+      username: 'admin',
+      id: 'admin',
+      admin: true
+    }
+
+    var options = {
+      expiresIn: 86400 // expires in 24 hours
+    }
+
+    var token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, options);
+
     test('GET /api/v2/appointments with an appointment saved in db', async () => {
       return request(app)
         .get('/api/v2/appointments')
+        .set('x-access-token', token)
         .expect('Content-Type', /json/)
         .expect(200)
         .then( (res) => {
@@ -66,6 +80,7 @@ describe('GET /api/v2/appointments', () => {
     test('GET /api/v2/appointments/:id with correct id', async () => {
         return request(app)
           .get('/api/v2/appointments/629b18ca308b2597242e3ee4')
+          .set('x-access-token', token)
           .expect('Content-Type', /json/)
           .expect(200, {
                 self: "/api/v2/appointments/629b18ca308b2597242e3ee4",
@@ -81,6 +96,7 @@ describe('GET /api/v2/appointments', () => {
       test('GET /api/v2/appointments/:id with incorrect id', async () => {
         return request(app)
           .get('/api/v2/appointments/629b18ca308b2597242e3ee5')
+          .set('x-access-token', token)
           .expect('Content-Type', /json/)
           .expect(404, {error: "Appointment not found"});
       });
